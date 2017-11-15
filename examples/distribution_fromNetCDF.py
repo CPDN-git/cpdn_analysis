82c15bf954ee4ead0b8aed033c60160a9e1b80fb
Sarah Sparrow <Sarah@Sarahs-MacBook-Air-5.local>
2017-11-15 10:48:12 +0000

    Generalising code

--- examples/distribution_fromNetCDF.py
+++ examples/distribution_fromNetCDF.py
@@ -21,14 +21,29 @@ cdo = Cdo()
 ddir="/group_workspaces/jasmin/cssp_china/users/ssparrow01/extracted/"
 os_dir="/home/users/ssparrow01/os_split/"
 bfolder="/region/"
-output={"Precip":("item5216_daily_mean","(mm/day)"),"Tmax":("item3236_daily_maximum","(K)"),"Tmin":("item3236_daily_minimum","(K)")}
+output={"Precip":("item5216_daily_mean","(mm/day)"),"Tmax":("item3236_daily_maximum","(K)"),"Tmin":("item3236_daily_minimum","(K)"),"Tmean":("item3236_daily_mean","(K)")}
 
-def read_data(batch,diag,os):
+def read_data(batch,diag,os,resolution,subregion):
     files=glob.glob(ddir+"batch_"+str(batch)+bfolder+output[diag][0]+"/*.nc")
     mean_vals=[]
     # Read the dictionary of the run OS
     run_os=read_os_run(os_dir+"batch_"+str(batch)+"_os_results.csv")
     
+    # Work out the input string for cdo
+    if resolution == '50km':
+	remap=' -remapbil,r720x360'
+    elif resolution == '25km':
+	remap=' -remapbil,r1440x730'
+    else:
+	remap=''
+
+    if subregion != '':
+	subreg=' -sellonlatbox,'+subregion
+    else:
+	subreg=''
+
+    inputstr=subreg+remap
+
     for ifile in files:
 	file_split=ifile.split("/")
 	filename=file_split[-1]
@@ -39,14 +54,14 @@ def read_data(batch,diag,os):
 	if run_os[umid]==os:
     		# Compute the field mean value timeseries and return it as a numpy array
     		if diag=="Precip":
-			vals=cdo.fldmean(input=" -sellonlatbox,110,117,25,35 -remapbil,r720x360 "+ifile,returnCdf=True).variables[output[diag][0]][:] 
+			vals=cdo.fldmean(input=inputstr+" "+ifile,returnCdf=True).variables[output[diag][0]][:] 
                 	vals = vals.flatten()
                 	vals=vals*86400
                 	mean_val=np.sum(vals)
 		else:
-			vals=cdo.fldmean(input=" -sellonlatbox,110,117,25,35 -remapbil,r720x360 "+ifile,returnCdf=True).variables[output[diag][0]][:] 	
+			vals=cdo.fldmean(input=inputstr+" "+ifile,returnCdf=True).variables[output[diag][0]][:] 	
 			vals = vals.flatten()
-			mean_val=np.max(vals)
+			mean_val=np.sum(vals)
 		mean_vals.append(mean_val)
     return mean_vals
 
@@ -63,7 +78,7 @@ def read_os_run(filename):
     f.close()
     return run_os 
 
-def plot_distribution_data(diag,batch_no): 
+def plot_distribution_data(diag,batch_no,res,subr): 
 
     # Set up the plot
     font = {'family' : 'sans-serif',
@@ -80,29 +95,31 @@ def plot_distribution_data(diag,batch_no):
     plt.setp(ax.get_yticklabels(),fontsize=12)
 
     # Read in the data
-    data_win=read_data(batch_no,diag,"Windows")
-    data_lin=read_data(batch_no,diag,"Linux")
-    data_mac=read_data(batch_no,diag,"Mac")
+    data_win=read_data(batch_no,diag,"Windows",res,subr)
+    data_lin=read_data(batch_no,diag,"Linux",res,subr)
+    data_mac=read_data(batch_no,diag,"Mac",res,subr)
     
     # PLot the data
     sns.kdeplot(np.array(data_win), shade=True, color="MediumBlue",label="Windows")
     sns.kdeplot(np.array(data_lin), shade=True, color="SeaGreen",label="Linux")
     sns.kdeplot(np.array(data_mac), shade=True, color="Orange",label="Mac")
 
-    ax.set_title(diag+" Distribution")
+    ax.set_title("Batch "+str(batch_no)+" "+diag+" Distribution")
 
     ll=ax.legend(loc="upper right",prop={"size": 12},fancybox=True,numpoints=1)
 
-    fig.savefig("distribution_"+diag+".png",dpi=28.75*2)
+    fig.savefig("Batch_"+str(batch_no)+"_distribution_"+diag+".png",dpi=28.75*2)
 
 	
 #Main controling function
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("variable", help="The diagnostic variable to look at Precip, Tmax or Tmin")
+    parser.add_argument("variable", help="The diagnostic variable to look at Precip, Tmean, Tmax or Tmin")
     parser.add_argument("batch", help="The batch number")
+    parser.add_argument("region_resolution", default="", help="The regional model resolution, 50km or 25km")
+    parser.add_argument("subregion", default="", help="The sub region of interest as lon1,lon2,lat1,lat2")
     args = parser.parse_args()
-    plot_distribution_data(args.variable,args.batch)
+    plot_distribution_data(args.variable,args.batch,args.region_resolution,args.subregion)
     print 'Finished!'
 
 #Washerboard function that allows main() to run on running this file
