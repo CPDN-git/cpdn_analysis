df2290a5099a9cc3c95ec326695ec03bef4e924c
Sarah Sparrow <Sarah@Sarahs-MacBook-Air-5.local>
2017-11-16 14:04:46 +0000

    Code has been generalised futher

--- examples/return_time_fromNetCDF.py
+++ examples/return_time_fromNetCDF.py
@@ -20,34 +20,63 @@ EU = os.path.expanduser
 sys.path.append(EU("../"))
 from return_time_plot import *
 
-ddir="/gpfs/projects/cpdn/scratch/Khaled/"
+ddir="/group_workspaces/jasmin/cssp_china/users/ssparrow01/extracted/"
+os_dir="/home/users/ssparrow01/os_split/"
 bfolder="/region/"
-output={"Precip":("item5216_daily_mean","(mm/day)"),"Tmax":("item3236_daily_maximum","(K)"),"Tmin":("item3236_daily_minimum","(K)")}
+output={"Precip":("item5216_daily_mean","(mm/day)"),"Tmax":("item3236_daily_maximum","(K)"),"Tmin":("item3236_daily_minimum","(K)"),"Tmean":("item3236_daily_mean","(K)")}
+timeargs=["mean","max","min","sum"]
+varname_dict={"Precip":("Pr","Prx","Prn","PrSum"),"Tmax":("Tmax","TXx","TXn","TmaxSum"),"Tmin":("Tmin","TNx","TNn","TminSum"), "Tmean":("T","Tx","Tn","Tsum")}
 
-def read_data(batch,diag):
-    files=glob.glob(ddir+"batch_"+str(batch)+bfolder+output[diag][0]+"/subset_*.nc")
+cols=[["MediumBlue","RoyalBlue"],["SeaGreen","SpringGreen"],["Orange","Gold"],["Indigo","MediumSlateBlue"]]
+
+def read_data(batch,diag,time_proc, os_run,resolution,subregion):
+    files=glob.glob(ddir+"batch_"+str(batch)+bfolder+output[diag][0]+"/*.nc")
     mean_vals=[]
-    
     # Read the dictionary of the run OS
-    run_os=read_os_run("/home/sarah.sparrow/batch_"+str(batch)+"_os_results.csv")
-    
+    run_os=read_os_run(os_dir+"batch_"+str(batch)+"_os_results.csv")
+
+    # Work out the input string for cdo
+    if resolution == '50km':
+        remap=' -remapbil,r720x360'
+    elif resolution == '25km':
+        remap=' -remapbil,r1440x730'
+    else:
+        remap=''
+
+    if subregion != '':
+        subreg=' -sellonlatbox,'+subregion
+    else:
+        subreg=''
+
+    inputstr=subreg+remap
+
     for ifile in files:
-	file_split=ifile.split("/")
-	filename=file_split[-1]
-	filename_split=filename.split("_")
-	umid=filename_split[4]
-	# Check the OS workunit was run on and reject if not windows
-	if run_os[umid]=="Windows":
-    		# Compute the field mean value timeseries and return it as a numpy array
-    		if diag=="Precip":
-			vals=cdo.fldmean(input=ifile,returnCdf=True).variables[output[diag][0]][:] 
-                	vals = vals.flatten()
-                	vals=vals*86400
-                	mean_val=np.sum(vals)
-		else:
-			vals=cdo.fldmean(input=ifile,returnCdf=True).variables[output[diag][0]][:] 	
-			vals = vals.flatten()
-			mean_val=np.mean(vals)
+        file_split=ifile.split("/")
+        filename=file_split[-1]
+        filename_split=filename.split("_")
+        umid=filename_split[3]
+        print umid
+        # Check the OS workunit was run on and reject if not windows
+        if run_os[umid] in os_run:
+		print run_os[umid]
+                # Compute the field mean value timeseries and return it as a numpy array
+                vals=cdo.fldmean(input=inputstr+" "+ifile,returnCdf=True).variables[output[diag][0]][:]
+
+		# Convert to mm/day for Precip
+                if diag=="Precip":
+                        vals=vals*86400
+
+                vals = vals.flatten()
+ 		# Perform the required time processing on the data
+                if time_proc =="mean":
+                	mean_val=np.mean(vals)
+                elif time_proc =="max":
+                        mean_val=np.max(vals)
+                elif time_proc =="min":
+                        mean_val==np.min(vals)
+                elif time_proc =="sum":
+                        mean_val=np.sum(vals)
+ 
 		mean_vals.append(mean_val)
     return mean_vals
 
@@ -79,7 +108,7 @@ def plot_data(data,cols,plabel,ax):
     cl1=ax.fill_between(x_data_all,conf_all_5,conf_all_95,facecolor=cols[1],edgecolor=cols[1],alpha=0.3,linewidth=1.5,zorder=4)
 
  
-def plot_return_time_data(diag): 
+def plot_return_time_data(diag,time_proc,batches,bnames,res,subr): 
 
     # Set up the plot
     font = {'family' : 'sans-serif',
@@ -96,34 +125,36 @@ def plot_return_time_data(diag):
     plt.setp(ax.get_yticklabels(),fontsize=12)
 
     # Read in the data
-    batch_act=631
-    batch_nat=632
-    
-    data_act=read_data(batch_act,diag)
-    data_nat=read_data(batch_nat,diag)
-    
-    plot_data(np.array(data_act),["MediumBlue","RoyalBlue"],"Actual",ax)
+    batch_list = batches.split(",")
+    batch_names = bnames.split(",")
 
-    plot_data(np.array(data_nat),["SeaGreen","SpringGreen"],"Natural",ax)
+    for ib, batch_no in enumerate(batch_list):
+    	data_win=read_data(batch_no,diag,time_proc,["Windows"],res,subr)
+    	plot_data(np.array(data_win),cols[ib],batch_names[ib],ax)
 
-    ax.set_title(diag+" Return Times")
+    dtitle=varname_dict[diag][timeargs.index(time_proc)]
+    ax.set_title(dtitle+" Return Times")
 
-    #ax.set_ylim(200,400)
-    ax.set_xlim(1.,1.e4)
-    labels=['','1/1','1/10','1/100','1/1000','']
+    ax.set_xlim(1.,1.e5)
+    labels=['','1/1','1/10','1/100','1/1000','1/10000','']
     ax.set_xticklabels(labels)
 
     ll=ax.legend(loc="lower right",prop={"size": 12},fancybox=True,numpoints=1)
 
-    fig.savefig("return_time_"+diag+".png",dpi=28.75*2)
+    fig.savefig("return_time_"+dtitle+".png",dpi=28.75*2)
 
 	
 #Main controling function
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("variable", help="The diagnostic variable to look at Precip, Tmax or Tmin")
+    parser.add_argument("variable", help="The diagnostic variable: Precip, Tmean, Tmax or Tmin")
+    parser.add_argument("time_process", help="The time processing required: mean, max, min or sum") 
+    parser.add_argument("batch", help="The list of batch numbers to plot, entered as eg '507,508,509'")
+    parser.add_argument("batch_name",help="The batch names to use entered as eg 'Actual,Natural,GHG only''")
+    parser.add_argument("region_resolution", default="", help="The regional model resolution: '50km' or '25km'")
+    parser.add_argument("subregion", default="", help="The sub region of interest as 'lon1,lon2,lat1,lat2'")
     args = parser.parse_args()
-    plot_return_time_data(args.variable)
+    plot_return_time_data(args.variable,args.time_process,args.batch,args.batch_name,args.region_resolution,args.subregion)
     print 'Finished!'
 
 #Washerboard function that allows main() to run on running this file
